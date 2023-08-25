f7cf30e3044e309d1bbb6cf2d2a635fb98059c95
8kyu <guillemsenabre@gmail.com>
2023-08-25 16:47:02 +0200

    Circle deleted | Added skills page

---
+++ frontend/src/components/Skills.js
@@ -0,0 +1,11 @@
+import '../styles/Skills.css';
+
+function Skills () {
+  return (
+    <div className = "skillsContainer">
+      <h1 className ="textSkills">SKILLS</h1>
+    </div>
+  );
+};
+
+export default Skills;
