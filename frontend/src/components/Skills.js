f57092ad75a2fb97fe0982e0c46221307099d39d
8kyu <guillemsenabre@gmail.com>
2023-08-27 00:09:47 +0200

    add skills components

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../styles/Skills.css';
 
+import Stats from './SkillsC/Stats.js';
+import About from './SkillsC/About.js';
+
 function Skills() {
   return (
     <div className="skillsContainer" id='skillsPage'>
-      <h1 className="textSkills">SKILLS</h1>
+      <Stats />
+      <About />
+
+      
     </div>
   );
 }
