23e7af095bff98fa2a960f9f88292dda5ba9c5a5
8kyu <guillemsenabre@gmail.com>
2023-10-13 14:42:05 +0800

    hyperlinks added | gmail copy done

--- frontend/src/components/Icons.js
+++ frontend/src/components/Icons.js
@@ -1,10 +1,38 @@
 import '../styles/Icons.css';
+import {githubLink, email} from '../config.js';
+
+import React, {useState} from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard'
 
 function Icons ({showContact}) {
+    
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        setCopied(true);
+
+        setTimeout(() => {
+          setCopied(false);
+        }, 3000);
+      };
+
     return (
         <div className={`iconsContainer ${showContact ? 'fadeIn' : ''}`}>
-            <img src="/images/gitInv.png" alt = "Github Img" className='githubImg'/>
-            <img src="/images/gmailInv.png" alt = "Gmail Img" className='gmailImg'/>
+            <a href={githubLink}>
+                <img 
+                    src="/images/gitInv.png" 
+                    alt = "Github Img" 
+                    className='githubImg'/>
+            </a>
+            <CopyToClipboard text = {email} onCopy={handleCopy}>
+                <img 
+                    src="/images/gmailInv.png" 
+                    alt = "Gmail Img" 
+                    className='gmailImg'
+                    style={{ cursor: 'pointer' }}
+                />
+            </CopyToClipboard> 
+            {copied && <span style={{ color: 'white' }}>Email copied!</span>}
         </div>
     );
 }
---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const githubLink = 'https://github.com/BakiRhina';
+export const email = 'guillemsenabre@gmail.com';
+export const description = '';
